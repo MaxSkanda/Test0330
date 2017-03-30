f840f69c8761dc6e7efc95870fa647b1123b4f2c
Mskanda <mskanda@student.tgm.ac.at>
2017-03-30 13:35:00 +0200

    hallo skanda

---
+++ src/skanda/Test0330.java
@@ -0,0 +1,12 @@
+package skanda;
+
+public class Test0330 {
+	
+	public static void hallo(){
+		System.out.println("Guten Tag Hr. Skanda !!!!");
+	}
+	
+	public static void main(){
+		hallo();
+	}
+}
