f1020b7609376f819608b9e9e1441a92132ae50d
Mskanda <mskanda@student.tgm.ac.at>
2017-03-30 13:36:33 +0200

    hallo 5*skanda

--- src/skanda/Test0330.java
+++ src/skanda/Test0330.java
@@ -4,6 +4,10 @@ public class Test0330 {
 	
 	public static void hallo(){
 		System.out.println("Guten Tag Hr. Skanda !!!!");
+		System.out.println("Guten Tag Hr. Skanda !!!!");
+		System.out.println("Guten Tag Hr. Skanda !!!!");
+		System.out.println("Guten Tag Hr. Skanda !!!!");
+		System.out.println("Guten Tag Hr. Skanda !!!!");
 	}
 	
 	public static void main(){
